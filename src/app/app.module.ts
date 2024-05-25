18d970babde2540a15f9b1124904d0a78ce20c9e
Alvaro <alvarohj96sg@gmail.com>
2024-05-25 23:45:51 +0200

    Added date selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IngresarHorasComponent } from './ingresar-horas/ingresar-horas.component';
 import { ResumenHorasComponent } from './resumen-horas/resumen-horas.component';
 import { ControlAdministradorComponent } from './control-administrador/control-administrador.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,11 @@ import { ControlAdministradorComponent } from './control-administrador/control-a
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule
   ],
   providers: [ ],
   bootstrap: [AppComponent]
